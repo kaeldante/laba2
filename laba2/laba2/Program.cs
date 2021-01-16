53460ccd657345cfa28e3a5ec82b0e50c0b465a6
Danil Yavorskiy <63715687+kaeldante@users.noreply.github.com>
2021-01-16 12:04:20 +0600

    Add files via upload

---
+++ laba2/laba2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: 123
+ * Дата: 16.01.2021
+ * Время: 11:17
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+
+namespace laba2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("ВАРИАНТ 7 (порядковый номер 32): Запрашивает у пользователя радиус и выводит площадь круга:");
+			const double PI = 3.1415926535;
+			Console.WriteLine("Число Пи = 3.1415926535");
+			Console.Write("Введите радиус круга: ");
+		
+	
+			double r = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("S= " + PI * r * r);
+			
+
+			
+				
+				Console.WriteLine("Нажмите любую клавишу для выхода");
+			Console.ReadKey();
+			
+			
+		}
+	}
+}
